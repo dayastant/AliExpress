0eb876cb39b75923e14e0ab4028ad660473cfef0
Dayastan <dayastant@gmail.com>
2026-02-26 11:31:49 +0530

    basic setup

--- aliexpress/src/app/page.tsx
+++ aliexpress/src/app/page.tsx
@@ -1,13 +1,16 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <>
+    <div className="p-5">
+      <div className="w-100 flex justify-end">
+        <ThemeToggle/>
+      </div>
       <h1 className="font-barlow text-blue-500">Welcome our pages</h1>
-      <h1 className=" text-blue-500">Welcome our pages</h1>
 
       <Button variant="destructive">Click here</Button>
-    </>
+    </div>
   );
 }
