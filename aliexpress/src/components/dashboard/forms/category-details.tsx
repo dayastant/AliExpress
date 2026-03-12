7bdd001fef714afcecaf428198b4d5c907b9af33
Dayastan <dayastant@gmail.com>
2026-03-12 07:19:21 +0530

    Add validation messages

--- aliexpress/src/components/dashboard/forms/category-details.tsx
+++ aliexpress/src/components/dashboard/forms/category-details.tsx
@@ -1,24 +1,64 @@
-import { Category } from "@prisma/client"
-import {FC} from "react"
+"use client";
 
-// form handling utilities
+// React
+import { FC, useEffect } from "react";
+
+// Prisma model
+import { Category } from "@prisma/client";
+
+// Form handling utilities
 import * as z from "zod";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+// Schema
 import { CategoryFormSchema } from "@/lib/schemas";
 
+import { useRouter } from "next/navigation";
 
 interface CategoryDetailsProps {
-    data?: Category
+  data?: Category;
 }
 
-const CategoryDetails: FC<CategoryDetailsProps> = ({ data })=>{
+const CategoryDetails: FC<CategoryDetailsProps> = ({ data }) => {
 
-    const form = useForm<z.infer<typeof CategoryFormSchema>>();
-    return (
-        <div>
 
-        </div>
-    )
-}
+  // Form hook for managing form state and validation
+  const form = useForm<z.infer<typeof CategoryFormSchema>>({
+    mode: "onChange", // Form validation mode
+    resolver: zodResolver(CategoryFormSchema), // Resolver for form validation
+    defaultValues: {
+      // Setting default form values from data (if available)
+      name: data?.name,
+      image: data?.image ? [{ url: data?.image }] : [],
+      url: data?.url,
+      featured: data?.featured,
+    },
+  });
+
+  // Loading status based on form submission
+  const isLoading = form.formState.isSubmitting;
+
+  // Reset form values when data changes
+  useEffect(() => {
+    if (data) {
+      form.reset({
+        name: data?.name,
+        image: [{ url: data?.image }],
+        url: data?.url,
+        featured: data?.featured,
+      });
+    }
+  }, [data, form]);
+
+  // Submit handler for form submission
+  
+
+  return (
+   <div>
+
+   </div>
+  );
+};
 
 export default CategoryDetails;
