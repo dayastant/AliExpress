a3f0fffc2eeb27c9da74524c21394cbf85ef4aee
Dayastan <dayastant@gmail.com>
2026-03-13 23:30:41 +0530

    Add profile and category

--- aliexpress/src/app/dashboard/admin/categories/new/page.tsx
+++ aliexpress/src/app/dashboard/admin/categories/new/page.tsx
@@ -4,9 +4,13 @@ import React from "react";
 
 
 export default function AdminNewCategoriesPage() {
+    const CLOUDINARY_GLOUD_KEY = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
+
+    if(!CLOUDINARY_GLOUD_KEY ) return null;
+
     return (
         <div className="w-full">
-            <CategoryDetails />
+            <CategoryDetails cloudinary_key= {CLOUDINARY_GLOUD_KEY} />
         </div>
     )
 }
